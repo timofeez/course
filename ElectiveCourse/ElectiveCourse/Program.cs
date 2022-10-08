45f4ec328f2f89ca28a03530cf1c917c6aa738a4
kids23 <gfdfgd@gmail.com>
2022-10-08 12:51:42 +0300

    initial

---
+++ ElectiveCourse/ElectiveCourse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ElectiveCourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                int result;
+                if (int.TryParse(input, out result))
+                {
+                    Console.WriteLine("реально число, хорош");
+                }
+                else
+                {
+                    Console.WriteLine("не число:(");
+                }
+            }
+        }
+    }
+}
